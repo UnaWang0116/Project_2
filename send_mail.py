348358ab69af77f8a32a4bde89f34de8e456e283
王卉如 <una@wanghuirudeMacBook-Air.local>
2024-07-13 16:06:14 +0800

    save5

--- send_mail.py
+++ send_mail.py
@@ -3,23 +3,26 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
-msg = MIMEMultipart()
-#用dictionary去設定
-sender = "fantasticseven1204@gmail.com"
-reciver = "kiol726a@gmail.com"
-msg["From"] = sender
-msg["To"] = reciver
-header = Header("Test Send Email","utf-8")
-msg["Subject"] = header.encode() #做編碼
+alot_reciver = ["kiol726a@gmail.com","han998775@gmail.com"]
 
-body = "Love you bear this is email send from 寶"
-msg.attach(MIMEText(body)) 
-#mbody = MIMEText(body) 上面一行等於兩行
-#msg.attach(mbody)
-#ssl._create_default_https_context = ssl._create_unverified_context
-ssl_context = ssl.create_default_context()
-# google專用的port:465
-with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
-    server.login(sender,"vwof aizu vmxn edjg")
-    server.sendmail(sender,reciver,msg.as_string())
-print("success send email")
+for reciver in alot_reciver:
+    msg = MIMEMultipart()
+    #用dictionary去設定
+    sender = "fantasticseven1204@gmail.com"
+    msg["From"] = sender
+    msg["To"] = reciver
+    header = Header("Tomorrow dating","utf-8")
+    msg["Subject"] = header.encode() #做編碼
+
+    body = "I want to have a good date tomorrow loveyou Frank"
+    msg.attach(MIMEText(body)) 
+    #mbody = MIMEText(body) 上面一行等於兩行
+    #msg.attach(mbody)
+    #ssl._create_default_https_context = ssl._create_unverified_context
+    ssl_context = ssl.create_default_context()
+    # google專用的port:465
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
+        server.login(sender,"vwof aizu vmxn edjg")
+        server.sendmail(sender,reciver,msg.as_string())
+
+    print("success send email")
